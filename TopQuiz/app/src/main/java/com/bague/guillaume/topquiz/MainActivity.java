7ac1544fe6520f5e16a05f578757b5aec97fcd44
guillaume <bague.guillaume@live.fr>
2018-10-22 14:44:19 +0200

    referencing graphics items

--- TopQuiz/app/src/main/java/com/bague/guillaume/topquiz/MainActivity.java
+++ TopQuiz/app/src/main/java/com/bague/guillaume/topquiz/MainActivity.java
@@ -2,12 +2,24 @@ package com.bague.guillaume.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingTxt;
+    private EditText mNameInput;
+    private Button mPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGreetingTxt = findViewById(R.id.activity_main_greeting_txt);
+        mNameInput =  findViewById(R.id.activity_main_name_input);
+        mPlayButton = findViewById(R.id.activity_main_play_btn);
+
     }
 }
