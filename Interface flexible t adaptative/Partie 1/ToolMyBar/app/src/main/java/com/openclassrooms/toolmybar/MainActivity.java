b2ff8ba5b593e5363706dff952c3779f7fdcc551
guillaume <bague.guillaume@live.fr>
2018-12-07 12:09:33 +0100

    implement toobar methods

--- Interface flexible t adaptative/Partie 1/ToolMyBar/app/src/main/java/com/openclassrooms/toolmybar/MainActivity.java
+++ Interface flexible t adaptative/Partie 1/ToolMyBar/app/src/main/java/com/openclassrooms/toolmybar/MainActivity.java
@@ -2,6 +2,10 @@ package com.openclassrooms.toolmybar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action menu
+        switch (item.getItemId()){
+            case(R.id.app_bar_params):
+                Toast.makeText(this, "Params", Toast.LENGTH_SHORT).show();
+                return true;
+            case(R.id.app_bar_search):
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void configureToolbar(){
+        //get toolbar view in activity
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //set the toolbar
+        setSupportActionBar(toolbar);
     }
 }
