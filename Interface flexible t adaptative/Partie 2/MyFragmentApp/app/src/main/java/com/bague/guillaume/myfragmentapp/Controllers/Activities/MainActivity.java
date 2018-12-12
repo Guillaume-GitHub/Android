8388023dd72cdb7dad7ab8b39cd27926fb7c825d
guillaume <bague.guillaume@live.fr>
2018-12-12 11:19:15 +0100

    Create fragment + add static link between main_activity and fragment_main

--- Interface flexible t adaptative/Partie 2/MyFragmentApp/app/src/main/java/com/bague/guillaume/myfragmentapp/Controllers/Activities/MainActivity.java
+++ Interface flexible t adaptative/Partie 2/MyFragmentApp/app/src/main/java/com/bague/guillaume/myfragmentapp/Controllers/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bague.guillaume.myfragmentapp;
+package com.bague.guillaume.myfragmentapp.Controllers.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bague.guillaume.myfragmentapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
