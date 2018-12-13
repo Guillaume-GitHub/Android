619c55c83de93c25d6972cbd9239689ec3b8c6eb
guillaume <bague.guillaume@live.fr>
2018-12-13 09:18:15 +0100

    add FrameLayout and xml for tablette

--- Interface flexible t adaptative/Partie 2/MyFragmentApp/app/src/main/java/com/bague/guillaume/myfragmentapp/Controllers/Activities/DetailActivity.java
+++ Interface flexible t adaptative/Partie 2/MyFragmentApp/app/src/main/java/com/bague/guillaume/myfragmentapp/Controllers/Activities/DetailActivity.java
@@ -9,9 +9,34 @@ import com.bague.guillaume.myfragmentapp.R;
 
 public class DetailActivity extends AppCompatActivity{
 
+    private DetailFragment  mDetailFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        this.configureAndShowFragment();
+    }
+
+
+    //********
+    //FRAGMENT
+    //********
+
+    private void configureAndShowFragment(){
+
+        // A- Get FragmentManager (support) and Try to find an existing Fragment instance in FrameLayout container
+        mDetailFragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.frameLayout_activity_detail);
+
+        if (mDetailFragment == null){
+            //B- Create new instance of DetailFragment
+            mDetailFragment = new DetailFragment();
+            //C- Add it to FrameLayout container
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frameLayout_activity_detail, mDetailFragment)
+                    .commit();
+        }
+
     }
 }
--- Interface flexible t adaptative/Partie 2/MyFragmentApp/app/src/main/java/com/bague/guillaume/myfragmentapp/Controllers/Activities/MainActivity.java
+++ Interface flexible t adaptative/Partie 2/MyFragmentApp/app/src/main/java/com/bague/guillaume/myfragmentapp/Controllers/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bague.guillaume.myfragmentapp.Controllers.Activities;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,16 +14,41 @@ import com.bague.guillaume.myfragmentapp.R;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
 
+    //1 - Declare fragment
+    private MainFragment mMainFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.configureAndShowFragment();
     }
 
+    //*********
+    //CALL BACK
+    //*********
     @Override
     public void onFragmentInteraction(View view) {
-
         Log.d("Test", "onFragmentInteraction: click");
         startActivity(new Intent(this, DetailActivity.class));
+    }
+
+    //********
+    //FRAGMENT
+    //********
+
+    private void configureAndShowFragment(){
+        // A- Get FragmentManager (support) and Try to find an existing Fragment instance in FrameLayout container
+        mMainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.frameLayout_activity_main);
+
+        if (mMainFragment == null){
+            // B- Create new mainFragment
+            mMainFragment = new MainFragment();
+            // C- Add it to FrameLayout container
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frameLayout_activity_main,mMainFragment)
+                    .commit();
+        }
     }
 }
